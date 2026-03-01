fc5af521830efb8c7f8d5afa08dfafc72662b183
AllanRono1 <allanrono18@gmail.com>
2026-03-01 19:34:29 +0300

    REFACTORED thr render function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const compEl = document.getElementById("compscore-el");
 const playerEl = document.getElementById("playerscore-el");
 const buttonEl = document.getElementById("button-el")
 const resultsEl = document.getElementById("results-el")
+const localStorageItem = localStorage.getItem("myLeads")
+console.log(localStorageItem)
 let message = ""
 let playerScore = 0
 let computerScore = 0
@@ -13,28 +15,17 @@ let item2 = "scissors";
 let item3 = "paper"
 
 buttonEl.addEventListener("click", function() {
-    let results = ""
     myLeads.push(playerEl.value)
     playerEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    myLeads = localStorage.getItem("myLeads")
-    for(let i = 0; i < myLeads.length; i++) {
-        results = `<li> ${myLeads} </li>`
-    }
-
-    resultsEl.innerHTML = results
-
+    renderLeads()
 })
-function getWinner() {
-if(item1 === "rock" && item2 === "scissors") {
-    console.log("rock wins🤘") 
-} else if(item1 === "rock" && item3 === "paper") {
-    console.log("paper wins🧻")
-} else if(item2 === item3) {
-    console.log("scissors wins✂")
-} else {
-    console.log("not valid")
-}
-}
 
 
+function renderLeads() {
+    let display = ""
+    for(let i = 0; i < myLeads.length; i++) {
+        display +=  `<li>${myLeads[i]}</li>`
+    }
+    resultsEl.innerHTML = display;
+}
